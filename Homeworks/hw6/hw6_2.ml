c34b033b32bfce6352a915edc8d64d7f00cb457e
Kim Dongkyum <ehrdmf@gmail.com>
2013-11-19 16:08:29 +0900

    2013.11.19.16:08

--- Homeworks/hw6/hw6_2.ml
+++ Homeworks/hw6/hw6_2.ml
@@ -1,13 +1,30 @@
-type team = Korea | France | Usa | Brazil | Japan | Nigeria | Cameroon
+type team = | Korea | France | Usa | Brazil | Japan | Nigeria | Cameroon
           | Poland | Portugal | Italy | Germany | Norway | Sweden | England
           | Argentina
 
-type tourna = LEAF of team
-            | NODE of tourna * tourna
+type tourna =
+  | LEAF of team
+  | NODE of tourna * tourna
+
+let string_of_team (t:team) : string=
+ match t with
+  | Korea -> "Korea"
+  | France -> "France"
+  | Usa -> "Usa"
+  | Brazil -> "Brazil"
+  | Japan -> "Japan"
+  | Nigeria -> "Nigeria"
+  | Cameroon -> "Cameroon"
+  | Poland -> "Poland"
+  | Portugal -> "Portugal"
+  | Italy -> "Italy"
+  | Germany -> "Germany"
+  | Norway -> "Norway"
+  | Sweden -> "Sweden"
+  | England -> "England"
+  | Argentina -> "Argentina"
 
 let rec parenize (t: tourna): string =
   match t with
-    |LEAF v -> v
-    |NODE (l, r) -> "(" ^ (parenize l) ^ " " ^ (parenize r) ^ ")"
-
-let ex=parenize(NODE(NODE(LEAF Korea, LEAF Portugal), LEAF Brazil));;
+    | LEAF v -> string_of_team v
+    | NODE (l, r) -> "(" ^ (parenize l) ^ " " ^ (parenize r) ^ ")"
